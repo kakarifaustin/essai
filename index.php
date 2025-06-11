9be2a92b5311e67a93dc82270e617d9126293470
FaustinAsyst <fkarikurubu@asystresources.com>
2025-06-11 18:08:21 +0200

    style

--- index.php
+++ index.php
@@ -17,9 +17,38 @@
         <input type="submit" value="S'inscrire">
     </form>
 <style>
-    form{
-        background-color: green;
-    }
+        body {
+            background-color: green;
+            font-family: Arial, sans-serif;
+        }
+
+        form {
+            background-color: white;
+            padding: 20px;
+            width: 400px;
+            border-radius: 10px;
+           
+        }
+
+        .form-group {
+            display: flex;
+            margin-bottom: 15px;
+            align-items: center;
+        }
+
+        .form-group label {
+            width: 120px;
+            font-weight: bold;
+        }
+
+        .form-group input {
+            flex: 1;
+            padding: 5px;
+        }
+
+        
+       
+    
 </style>
 </body>
 
